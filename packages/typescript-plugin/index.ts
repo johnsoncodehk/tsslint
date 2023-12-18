c304edf209f9962787d363299785e64fe487f3f3
Johnson Chu <johnsoncodehk@gmail.com>
2023-12-18 18:19:05 +0800

    fix(typescript-plugin): avoid duplication of decorations

--- packages/typescript-plugin/index.ts
+++ packages/typescript-plugin/index.ts
@@ -4,45 +4,135 @@ import { watchConfig } from './lib/watchConfig';
 import { builtInPlugins } from './lib/builtInPlugins';
 import * as path from 'path';
 
+const languageServiceDecorator = new WeakMap<ts.LanguageService, ReturnType<typeof decorateLanguageService>>();
+
 const init: ts.server.PluginModuleFactory = (modules) => {
 	const { typescript: ts } = modules;
 	const pluginModule: ts.server.PluginModule = {
 		create(info) {
 
-			const tsconfig = info.project.projectKind === ts.server.ProjectKind.Configured
-				? info.project.getProjectName()
-				: undefined;
-			if (!tsconfig) {
-				return info.languageService;
+			if (!languageServiceDecorator.has(info.languageService)) {
+				const tsconfig = info.project.projectKind === ts.server.ProjectKind.Configured
+					? info.project.getProjectName()
+					: undefined;
+				if (tsconfig) {
+					languageServiceDecorator.set(
+						info.languageService,
+						decorateLanguageService(
+							ts,
+							tsconfig,
+							info.languageServiceHost,
+							info.languageService,
+						),
+					);
+				}
 			}
 
-			if (!info.config.configFile) {
-				return info.languageService;
+			languageServiceDecorator.get(info.languageService)?.update(info);
+
+			return info.languageService;
+		},
+	};
+	return pluginModule;
+};
+
+export = init;
+
+function decorateLanguageService(
+	ts: typeof import('typescript/lib/tsserverlibrary'),
+	tsconfig: string,
+	languageServiceHost: ts.LanguageServiceHost,
+	languageService: ts.LanguageService,
+) {
+
+	const getCompilerOptionsDiagnostics = languageService.getCompilerOptionsDiagnostics;
+	const getSyntacticDiagnostics = languageService.getSyntacticDiagnostics;
+	const getApplicableRefactors = languageService.getApplicableRefactors;
+	const getEditsForRefactor = languageService.getEditsForRefactor;
+
+	let compilerOptionsDiagnostics: ts.Diagnostic[] = [];
+	let configFile: string | undefined;
+	let configFileBuildContext: Awaited<ReturnType<typeof watchConfig>> | undefined;
+	let config: Config | undefined;
+	let plugins: PluginInstance[] = [];
+
+	languageService.getCompilerOptionsDiagnostics = () => {
+		return getCompilerOptionsDiagnostics().concat(compilerOptionsDiagnostics);
+	};
+	languageService.getSyntacticDiagnostics = fileName => {
+
+		let errors: ts.Diagnostic[] = getSyntacticDiagnostics(fileName);
+
+		const sourceFile = languageService.getProgram()?.getSourceFile(fileName);
+		if (!sourceFile) {
+			return errors as ts.DiagnosticWithLocation[];
+		}
+
+		const token = languageServiceHost.getCancellationToken?.();
+
+		for (const plugin of plugins) {
+			if (token?.isCancellationRequested()) {
+				break;
 			}
-			let configFile: string | undefined;
-			try {
-				configFile = require.resolve(info.config.configFile, { paths: [path.dirname(tsconfig)] });
-			} catch (err) {
-				const getCompilerOptionsDiagnostics = info.languageService.getCompilerOptionsDiagnostics;
-
-				info.languageService.getCompilerOptionsDiagnostics = () => {
-					const configFile = ts.readJsonConfigFile(tsconfig, ts.sys.readFile);
-					const start = configFile.text.indexOf(info.config.configFile);
-					return getCompilerOptionsDiagnostics().concat([{
-						category: ts.DiagnosticCategory.Warning,
-						code: 0,
-						messageText: String(err),
-						file: configFile,
-						start: start - 1,
-						length: info.config.configFile.length + 2,
-					}]);
-				};
-
-				return info.languageService;
+			if (plugin.lint) {
+				let pluginResult = plugin.lint?.(sourceFile, config?.rules ?? {});
+				for (const plugin of plugins) {
+					if (plugin.resolveResult) {
+						pluginResult = plugin.resolveResult(pluginResult);
+					}
+				}
+				errors = errors.concat(pluginResult);
 			}
+		}
 
-			let config: Config | undefined;
-			let plugins: PluginInstance[] = [];
+		return errors as ts.DiagnosticWithLocation[];
+	};
+	languageService.getApplicableRefactors = (fileName, positionOrRange, ...rest) => {
+
+		let refactors = getApplicableRefactors(fileName, positionOrRange, ...rest);
+
+		const sourceFile = languageService.getProgram()?.getSourceFile(fileName);
+		if (!sourceFile) {
+			return refactors;
+		}
+
+		const token = languageServiceHost.getCancellationToken?.();
+
+		for (const plugin of plugins) {
+			if (token?.isCancellationRequested()) {
+				break;
+			}
+			refactors = refactors.concat(plugin.getFixes?.(sourceFile, positionOrRange) ?? []);
+		}
+
+		return refactors;
+	};
+	languageService.getEditsForRefactor = (fileName, formatOptions, positionOrRange, refactorName, actionName, ...rest) => {
+
+		const sourceFile = languageService.getProgram()?.getSourceFile(fileName);
+		if (!sourceFile) {
+			return;
+		}
+
+		for (const plugin of plugins) {
+			const edits = plugin.fix?.(sourceFile, refactorName, actionName);
+			if (edits) {
+				return { edits };
+			}
+		}
+
+		return getEditsForRefactor(fileName, formatOptions, positionOrRange, refactorName, actionName, ...rest);
+	};
+
+	return { update };
+
+	async function update(info: ts.server.PluginCreateInfo) {
+		try {
+			const newConfigFile = require.resolve(info.config.configFile, { paths: [path.dirname(tsconfig)] });
+			if (newConfigFile === configFile) {
+				return;
+			}
+			configFile = newConfigFile;
 
 			const projectContext: ProjectContext = {
 				configFile,
@@ -52,9 +142,8 @@ const init: ts.server.PluginModuleFactory = (modules) => {
 				typescript: ts,
 			};
 
-			decorateLanguageService();
-
-			watchConfig(
+			configFileBuildContext?.dispose();
+			configFileBuildContext = await watchConfig(
 				configFile,
 				async (_config, result) => {
 					config = _config;
@@ -72,83 +161,17 @@ const init: ts.server.PluginModuleFactory = (modules) => {
 					info.project.refreshDiagnostics();
 				},
 			);
-
-			return info.languageService;
-
-			function decorateLanguageService() {
-
-				const getSyntacticDiagnostics = info.languageService.getSyntacticDiagnostics;
-				const getApplicableRefactors = info.languageService.getApplicableRefactors;
-				const getEditsForRefactor = info.languageService.getEditsForRefactor;
-
-				info.languageService.getSyntacticDiagnostics = fileName => {
-
-					let errors: ts.Diagnostic[] = getSyntacticDiagnostics(fileName);
-
-					const sourceFile = info.languageService.getProgram()?.getSourceFile(fileName);
-					if (!sourceFile) {
-						return errors as ts.DiagnosticWithLocation[];
-					}
-
-					const token = info.languageServiceHost.getCancellationToken?.();
-
-					for (const plugin of plugins) {
-						if (token?.isCancellationRequested()) {
-							break;
-						}
-						if (plugin.lint) {
-							let pluginResult = plugin.lint?.(sourceFile, config?.rules ?? {});
-							for (const plugin of plugins) {
-								if (plugin.resolveResult) {
-									pluginResult = plugin.resolveResult(pluginResult);
-								}
-							}
-							errors = errors.concat(pluginResult);
-						}
-					}
-
-					return errors as ts.DiagnosticWithLocation[];
-				};
-				info.languageService.getApplicableRefactors = (fileName, positionOrRange, ...rest) => {
-
-					let refactors = getApplicableRefactors(fileName, positionOrRange, ...rest);
-
-					const sourceFile = info.languageService.getProgram()?.getSourceFile(fileName);
-					if (!sourceFile) {
-						return refactors;
-					}
-
-					const token = info.languageServiceHost.getCancellationToken?.();
-
-					for (const plugin of plugins) {
-						if (token?.isCancellationRequested()) {
-							break;
-						}
-						refactors = refactors.concat(plugin.getFixes?.(sourceFile, positionOrRange) ?? []);
-					}
-
-					return refactors;
-				};
-				info.languageService.getEditsForRefactor = (fileName, formatOptions, positionOrRange, refactorName, actionName, ...rest) => {
-
-					const sourceFile = info.languageService.getProgram()?.getSourceFile(fileName);
-					if (!sourceFile) {
-						return;
-					}
-
-					for (const plugin of plugins) {
-						const edits = plugin.fix?.(sourceFile, refactorName, actionName);
-						if (edits) {
-							return { edits };
-						}
-					}
-
-					return getEditsForRefactor(fileName, formatOptions, positionOrRange, refactorName, actionName, ...rest);
-				};
-			}
-		},
-	};
-	return pluginModule;
-};
-
-export = init;
+		} catch (err) {
+			const configFile = ts.readJsonConfigFile(tsconfig, ts.sys.readFile);
+			const start = configFile.text.indexOf(info.config.configFile);
+			compilerOptionsDiagnostics = [{
+				category: ts.DiagnosticCategory.Warning,
+				code: 0,
+				messageText: String(err),
+				file: configFile,
+				start: start - 1,
+				length: info.config.configFile.length + 2,
+			}];
+		}
+	}
+}
