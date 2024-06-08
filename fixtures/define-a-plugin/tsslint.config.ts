42619b9a5172010288e93bf3b642c438a3132aee
Johnson Chu <johnsoncodehk@gmail.com>
2024-06-09 00:52:07 +0800

    Update tsslint.config.ts

--- fixtures/define-a-plugin/tsslint.config.ts
+++ fixtures/define-a-plugin/tsslint.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from '@tsslint/config';
-import noConsoleRule from '../noConsoleRule';
+import { create as createNoConsoleRule } from '../noConsoleRule';
 
 export default defineConfig({
 	plugins: [
 		() => ({
 			resolveRules(rules) {
-				rules['no-console'] = noConsoleRule;
+				rules['no-console'] = createNoConsoleRule();
 				return rules;
 			},
 		}),
