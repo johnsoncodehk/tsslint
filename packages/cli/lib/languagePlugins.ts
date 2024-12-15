8cd164851426b3a059da3dcdba52a9d5a5c1a8c9
Johnson Chu <johnsoncodehk@gmail.com>
2024-12-15 17:03:00 +0800

    Support `--vue-projects` flag

--- packages/cli/lib/languagePlugins.ts
+++ packages/cli/lib/languagePlugins.ts
@@ -1,8 +1,27 @@
 import { LanguagePlugin } from '@volar/language-core';
 import path = require('path');
 import ts = require('typescript');
+import glob = require('glob');
 
 const cache = new Map<string, LanguagePlugin<string>[]>();
+const vueProjects = new Set<string>();
+
+if (process.argv.includes('--vue-projects')) {
+	const projectsIndex = process.argv.indexOf('--vue-projects');
+	for (let i = projectsIndex + 1; i < process.argv.length; i++) {
+		if (process.argv[i].startsWith('-')) {
+			break;
+		}
+		const searchGlob = process.argv[i];
+		const tsconfigs = glob.sync(searchGlob);
+		for (let tsconfig of tsconfigs) {
+			if (!tsconfig.startsWith('.')) {
+				tsconfig = `./${tsconfig}`;
+			}
+			vueProjects.add(require.resolve(tsconfig, { paths: [process.cwd()] }));
+		}
+	}
+}
 
 export function load(tsconfig: string) {
 	if (cache.has(tsconfig)) {
@@ -10,7 +29,7 @@ export function load(tsconfig: string) {
 	}
 	const plugins: LanguagePlugin<string>[] = [];
 
-	if (process.argv.includes('--enable-vue-support')) {
+	if (vueProjects.has(tsconfig)) {
 		let vue: typeof import('@vue/language-core');
 		let vueTscPkgPath: string | undefined;
 
@@ -24,7 +43,7 @@ export function load(tsconfig: string) {
 			if (pkg) {
 				throw new Error('Please install @vue/language-core or vue-tsc to ' + path.relative(process.cwd(), pkg));
 			} else {
-				throw new Error('Please install @vue/language-core or vue-tsc');
+				throw new Error('Please install @vue/language-core or vue-tsc for ' + path.relative(process.cwd(), tsconfig));
 			}
 		}
 
