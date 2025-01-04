c65906fe743ef92e5e776dca9ab07107b13dd69e
Johnson Chu <johnsoncodehk@gmail.com>
2025-01-05 00:35:56 +0800

    fix(tslint): tolerating missing metadata

--- packages/tslint/index.ts
+++ packages/tslint/index.ts
@@ -3,16 +3,18 @@ import type * as TSLint from 'tslint';
 import { WalkContext } from 'tslint/lib/language/walker';
 import type * as ts from 'typescript';
 
-export function convertRule<T extends import('tslint/lib/language/rule/rule').RuleConstructor>(
+type TSLintRule = import('tslint/lib/language/rule/rule').RuleConstructor;
+
+export function convertRule<T extends Partial<TSLintRule> | TSLintRule>(
 	Rule: T,
 	ruleArguments: any[] = [],
 	severity: ts.DiagnosticCategory =
-		Rule.metadata.type === 'functionality' || Rule.metadata.type === 'typescript' ? 1 satisfies ts.DiagnosticCategory.Error
+		!Rule.metadata || Rule.metadata.type === 'functionality' || Rule.metadata.type === 'typescript' ? 1 satisfies ts.DiagnosticCategory.Error
 			: Rule.metadata.type === 'maintainability' || Rule.metadata.type === 'style' ? 0 satisfies ts.DiagnosticCategory.Warning
 				: Rule.metadata.type === 'formatting' ? 2 satisfies ts.DiagnosticCategory.Suggestion
 					: 3 satisfies ts.DiagnosticCategory.Message
 ): TSSLint.Rule {
-	const rule = new Rule({
+	const rule = new (Rule as TSLintRule)({
 		ruleName: '',
 		ruleArguments,
 		ruleSeverity: severity === 1 ? 'error' : severity === 2 ? 'warning' : 'off',
@@ -79,7 +81,7 @@ export function convertRule<T extends import('tslint/lib/language/rule/rule').Ru
 			walkFn(ctx, programOrChecker);
 			return ctx.failures;
 		};
-		Rule.metadata.requiresTypeInfo
+		Rule.metadata?.requiresTypeInfo
 			// @ts-expect-error
 			? rule.applyWithProgram(sourceFile, languageService.getProgram()) as TSLint.RuleFailure[]
 			: rule.apply(sourceFile);
