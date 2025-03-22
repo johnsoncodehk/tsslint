9fa2d2ca8a81d11e2237f4b338e1b105e37d46ae
Johnson Chu <johnsoncodehk@gmail.com>
2025-03-22 08:41:27 +0800

    feat(eslint): add convertFormattingRules API

--- packages/eslint/lib/types.ts
+++ packages/eslint/lib/types.ts
@@ -1,5 +1,5 @@
-type S = 'error' | 'warn' | 'suggestion' | 'off' | 0 | 1 | 2;
-type O<T extends any[]> = S | [S, ...options: T];
+export type S = 'error' | 'warn' | 'suggestion' | 'off' | 0 | 1 | 2;
+export type O<T extends any[]> = S | [S, ...options: T];
 
 export interface ESLintRulesConfig {
 	[key: string]: O<any[]>;
