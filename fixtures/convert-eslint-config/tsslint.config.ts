15681ab22bf3824201eaff54d017faab4585eb97
Johnson Chu <johnsoncodehk@gmail.com>
2025-08-11 22:09:20 +0800

    refactor(fixtures): convertConfig -> convertRules

--- fixtures/convert-eslint-config/tsslint.config.ts
+++ fixtures/convert-eslint-config/tsslint.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from '@tsslint/config';
-import { convertConfig } from '@tsslint/eslint';
+import { convertRules } from '@tsslint/eslint';
 
 export default defineConfig({
-	rules: convertConfig(((await import('eslint-plugin-expect-type')).configs).recommended.rules),
+	rules: await convertRules({
+		'expect-type/expect': 'error',
+	}),
 });
