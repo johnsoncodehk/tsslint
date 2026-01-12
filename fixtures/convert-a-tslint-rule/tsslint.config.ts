d559eee5730c88fda266962145aeff9715a296d0
Johnson Chu <johnsoncodehk@gmail.com>
2026-01-13 01:05:40 +0800

    update

--- fixtures/convert-a-tslint-rule/tsslint.config.ts
+++ fixtures/convert-a-tslint-rule/tsslint.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from '@tsslint/config';
-import { convertRule } from '@tsslint/tslint';
+import { defineRules } from '@tsslint/tslint';
 
 export default defineConfig({
 	rules: {
-		'strict-boolean-expressions': convertRule((await import('tslint/lib/rules/strictBooleanExpressionsRule.js')).Rule),
+		...await defineRules({
+			'strict-boolean-expressions': true,
+		}),
+		// 'strict-boolean-expressions': convertRule((await import('tslint/lib/rules/strictBooleanExpressionsRule.js')).Rule),
 	},
 });
