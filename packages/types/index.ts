e05416fcb2cbdeeea2bf76db2668a0850ca80cff
Johnson Chu <johnsoncodehk@gmail.com>
2026-01-15 10:05:57 +0800

    fix: add withoutCache API to Reporter (#75)

--- packages/types/index.ts
+++ packages/types/index.ts
@@ -56,4 +56,5 @@ export interface Reporter {
 	withUnnecessary(): Reporter;
 	withFix(title: string, getChanges: () => FileTextChanges[]): Reporter;
 	withRefactor(title: string, getChanges: () => FileTextChanges[]): Reporter;
+	withoutCache(): Reporter;
 }
