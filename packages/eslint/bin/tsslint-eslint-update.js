18760971ff89bae6bcef5e33ddc22b30f3554dc9
johnsoncodehk <16279759+johnsoncodehk@users.noreply.github.com>
2026-01-14 20:20:12 -0500

    refactor(eslint): simplify structure by merging generateDts.js into bin script

--- packages/eslint/bin/tsslint-eslint-update.js
+++ packages/eslint/bin/tsslint-eslint-update.js
@@ -1,2 +1,60 @@
 #!/usr/bin/env node
-require('../scripts/generateDts.js');
+
+const path = require('path');
+const fs = require('fs');
+const nodeModulesDirs = [];
+
+let dir = __dirname;
+
+while (true) {
+	const nodeModuleDir = path.join(dir, 'node_modules');
+	if (fs.existsSync(nodeModuleDir)) {
+		nodeModulesDirs.push(nodeModuleDir);
+	}
+	const parentDir = path.resolve(dir, '..');
+	if (parentDir === dir) {
+		break;
+	}
+	dir = parentDir;
+}
+
+try {
+	const { generate } = require('../lib/dtsGenerate.js');
+	generate(nodeModulesDirs).then(({ dts, stats }) => {
+		fs.writeFileSync(path.resolve(__dirname, '..', 'lib', 'types.d.ts'), dts);
+
+		const indexPath = path.resolve(__dirname, '..', 'index.d.ts');
+		if (fs.existsSync(indexPath)) {
+			let indexContent = fs.readFileSync(indexPath, 'utf8');
+			const defineRulesIndex = indexContent.indexOf('export declare function defineRules');
+			const jsDocEnd = indexContent.lastIndexOf('*/', defineRulesIndex) + 2;
+			const jsDocStart = indexContent.lastIndexOf('/**', jsDocEnd);
+
+			if (jsDocStart !== -1 && jsDocEnd !== -1 && jsDocStart < defineRulesIndex) {
+				const statsTable = [
+					'| Plugin | Rules |',
+					'| :--- | :--- |',
+					...Object.entries(stats)
+						.filter(([_, count]) => count > 0)
+						.sort((a, b) => b[1] - a[1])
+						.map(([name, count]) => `| <span>${name}</span> | ${count} |`),
+				].join('\n	 * ');
+
+				const newJsDoc = `/**
+	 * Converts an ESLint rules configuration to TSSLint rules.
+	 *
+	 * ${statsTable}
+	 *
+	 * ---
+	 * If you have added new ESLint plugins, please run \`npx tsslint-eslint-update\` to update this list.
+	 * 
+	 * ---
+	 */`;
+				indexContent = indexContent.slice(0, jsDocStart) + newJsDoc + indexContent.slice(jsDocEnd);
+				fs.writeFileSync(indexPath, indexContent);
+			}
+		}
+	});
+} catch (err) {
+	console.error(err);
+}
