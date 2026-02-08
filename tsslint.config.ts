3b7722b3dd42d8edf5bb1f51acfdd975f1eacd76
Johnson Chu <johnsoncodehk@gmail.com>
2026-02-09 00:43:13 +0800

    chore: use workspace packages in tsslint.config.ts

--- tsslint.config.ts
+++ tsslint.config.ts
@@ -1,11 +1,15 @@
-import { defineConfig, importESLintRules } from '@tsslint/config';
+import { defineConfig, importESLintRules } from './packages/config/index.js';
 
 export default defineConfig({
-	rules: await importESLintRules({
-		'@typescript-eslint/consistent-type-imports': [true, {
-			disallowTypeAnnotations: false,
-			fixStyle: 'inline-type-imports',
-		}],
-		'@typescript-eslint/no-unnecessary-type-assertion': true,
-	}),
+	rules: await importESLintRules(
+		{
+			'@typescript-eslint/consistent-type-imports': [true, {
+				disallowTypeAnnotations: false,
+				fixStyle: 'inline-type-imports',
+			}],
+			'@typescript-eslint/no-unnecessary-type-assertion': true,
+		},
+		{},
+		async () => (await import('./packages/compat-eslint/index.js')).convertRule,
+	),
 });
